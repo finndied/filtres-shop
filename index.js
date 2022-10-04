1f1967d61989a0f4410596fd6459d8bd7352efd6
Ilya Morozov <finnlivefx1@mail.ru>
2022-10-04 22:17:55 +0700

    Добавление parallax эффекта

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+//--------- BACKGROUND ------------ //
+
+const bgWater = document.getElementById("background-water");
+
+document.addEventListener("mousemove", function (e) {
+  MoveBackground(e);
+});
+
+function MoveBackground(e) {
+  let offsetX = (e.clientX / window.innerWidth) * 30 - 15;
+  let offsetY = (e.clientY / window.innerHeight) * 10 - 5;
+  bgWater.setAttribute(
+    "style",
+    "background-position: " + offsetX + "px " + offsetY + "px;"
+  );
+}
