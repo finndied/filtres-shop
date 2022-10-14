d8d848cc31429d96366c7e8eb9518f06842d0516
Ilya Morozov <finnlivefx1@mail.ru>
2022-10-14 18:02:36 +0700

    Верстка сабменю

--- index.js
+++ index.js
@@ -2,16 +2,82 @@
 
 // NAV-SUBMENU -------------------------------------------------------
 
-const subOpen = document.getElementById("1");
 const submenu = document.querySelector(".header__nav-submenu");
-const submenuBlock = document.querySelector(".header__nav-submenu-block");
-const subArrow = document.querySelector(".icon__dropdown");
 
-subOpen.onclick = function () {
-  submenu.classList.toggle("open");
-  submenuBlock.classList.toggle("open");
-  subArrow.classList.toggle("open");
+const arr1 = document.getElementById("arr1");
+const arr2 = document.getElementById("arr2");
+const arr3 = document.getElementById("arr3");
+const arr4 = document.getElementById("arr4");
+
+const block1 = document.getElementById("block1");
+const block2 = document.getElementById("block2");
+const block3 = document.getElementById("block3");
+const block4 = document.getElementById("block4");
+
+const items = document.querySelectorAll(".header__nav-child");
+for (let i = 0; i < items.length; i++) {
+  console.log(items);
+}
+
+// Функции для раскрывания сабменю при наведении
+
+items[0].onmouseover = function () {
+  submenu.classList.add("open");
+  arr1.classList.add("open");
+  block1.classList.add("open");
 };
+
+items[0].onmouseout = function () {
+  submenu.classList.remove("open");
+  arr1.classList.remove("open");
+  block1.classList.remove("open");
+};
+
+items[1].onmouseover = function () {
+  submenu.classList.add("open");
+  arr2.classList.add("open");
+  block2.classList.add("open");
+};
+
+items[1].onmouseout = function () {
+  submenu.classList.remove("open");
+  arr2.classList.remove("open");
+  block2.classList.remove("open");
+};
+items[2].onmouseover = function () {
+  submenu.classList.add("open");
+  arr3.classList.add("open");
+  block3.classList.add("open");
+};
+
+items[2].onmouseout = function () {
+  submenu.classList.remove("open");
+  arr3.classList.remove("open");
+  block3.classList.remove("open");
+};
+items[3].onmouseover = function () {
+  submenu.classList.add("open");
+  arr4.classList.add("open");
+  block4.classList.add("open");
+};
+
+items[3].onmouseout = function () {
+  submenu.classList.remove("open");
+  arr4.classList.remove("open");
+  block4.classList.remove("open");
+};
+
+// items[2].onclick = function menu1() {
+//   submenu.classList.toggle("open");
+//   subArrow.classList.toggle("open");
+//   submenuBlock.classList.toggle("open");
+// }
+
+// items[3].onclick = function menu1() {
+//   submenu.classList.toggle("open");
+//   subArrow.classList.toggle("open");
+//   submenuBlock.classList.toggle("open");
+// }
 
 // SLIDER ------------------------------------------------------------
 
